26c604a7519bed041bbec9a7275393615d45f40b
Kevin Garner <kevin.garner@ssigroup.com>
2024-04-15 15:11:40 -0500

    Added compatibility folder and a compatibility wire-up for Tidy 5e Sheets. Extracted and exported any shared functionality that was needed.

--- module/item-sheet.js
+++ module/item-sheet.js
@@ -16,6 +16,11 @@ function addTab(sheet, html, data) {
 		)
 	);
 	const body = html.find('.sheet-body');
+
+	if (body.length === 0) {
+		return;
+	}
+
 	body.append(tab);
 	if (html[0].classList.contains('app')) {
 		const width = sheet.options.width + 80;
@@ -33,16 +38,22 @@ function addTab(sheet, html, data) {
 	}
 
 	// Listeners
+	activateTabListeners(menu, sheet, tab);
+	activateTabContentListeners(sheet, tab);
+}
+
+function activateTabListeners(menu, sheet, tab) {
 	menu.on('click', () => setTimeout(() => sheet.setPosition({ height: 'auto' }), 0));
-	tab.find('input,select,textarea').on('change', () =>
-		Hooks.once('renderItemSheet', () => sheet.element.find('nav [data-tab=item-card]')[0].click())
-	);
+	tab.find('input,select,textarea').on('change', () => Hooks.once('renderItemSheet', () => sheet.element.find('nav [data-tab=item-card]')[0].click()));
+}
+
+export function activateTabContentListeners(sheet, tab) {
 	tab.find('button.file-picker').on('click', (ev) => sheet._activateFilePicker(ev));
 	const flipped = sheet.document.getFlag('item-cards-dnd5e', 'flipped');
 	tab.find('#card-preview').on('click', () => PopoutCard.createFromUuid({ uuid: sheet.item.uuid, flipped, preview: true }));
 }
 
-function specialMembership() {
+export function specialMembership() {
 	try {
 		const membership = getSetting('specialMembership');
 		return Boolean(membership && game.membership?.hasPermissionSync(membership));
